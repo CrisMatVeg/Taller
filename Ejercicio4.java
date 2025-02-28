5a9b1df5abf4bf9cad66511ed9604978fa7d1f71
Cristian Mateos <cristian.matveg@educa.jcyl.es>
2025-02-28 10:56:35 +0100

    Sistem error corregido

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -5,6 +5,6 @@ public class Ejercicio4{
 		int n1= myObj.nextInt();
 		int n2= myObj.nextInt();
 		int n3= myObj.nextInt();
-		Syste.out.println("Media Final = "+((n1+n2+n3)/3.0));
+		System.out.println("Media Final = "+((n1+n2+n3)/3.0));
 	}
 }
